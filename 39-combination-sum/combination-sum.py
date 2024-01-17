ee5207dbd9b3d97362bc1a34b98e5a8dcff9ac9c
Rahul Asati <89442495+RahulAsati123@users.noreply.github.com>
2024-01-17 12:53:35 +0530

    Time: 29 ms (100.00%) | Memory: 17.3 MB (39.62%) - LeetSync

--- 39-combination-sum/combination-sum.py
+++ 39-combination-sum/combination-sum.py
@@ -1,22 +1,15 @@
 class Solution:
     def ss(self,nums,arr,target,idx):
         if idx == len(nums):
-            if target == 0 :
-             
-                self.ans.append(arr.copy())
-                
+            if target == 0:
+               self.ans.append(arr.copy())
             return
-       
-        if nums[idx]<=target:
-         
-            arr.append(nums[idx])
-          
-            self.ss(nums,arr,target-nums[idx],idx)
-      
-            arr.pop(-1)
         
+        if nums[idx]<=target:
+            arr.append(nums[idx])
+            self.ss(nums,arr,target-nums[idx],idx)
+            arr.pop(-1)
         self.ss(nums,arr,target,idx+1)
-
 
     def combinationSum(self, nums: List[int], target: int) -> List[List[int]]:
         self.ans = []
