598ab88d1429e26f3441ee4b6ca15484f9df796d
Rahul Asati <89442495+RahulAsati123@users.noreply.github.com>
2024-01-17 12:59:56 +0530

    Time: 43 ms (96.01%) | Memory: 17.4 MB (36.80%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.py
+++ 40-combination-sum-ii/combination-sum-ii.py
@@ -2,17 +2,16 @@ class Solution:
     def ss(self,idx,arr,nums,target):
         if target == 0:
             self.ans.append(arr.copy())
-            return
+            return 
         
         for i in range(idx,len(nums)):
             if i>idx and nums[i] == nums[i-1]:
                 continue
-            if nums[i] > target:
+            if nums[i]> target:
                 break
             arr.append(nums[i])
             self.ss(i+1,arr,nums,target-nums[i])
             arr.pop(-1)
-            
 
         
     def combinationSum2(self, nums: List[int], target: int) -> List[List[int]]:
