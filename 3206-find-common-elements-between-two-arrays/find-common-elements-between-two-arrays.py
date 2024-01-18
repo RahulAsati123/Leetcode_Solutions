ee68d066a1a935ae795bfab985c069e07d94f118
Rahul Asati <89442495+RahulAsati123@users.noreply.github.com>
2024-01-18 23:05:24 +0530

    Time: 190 ms (9.20%) | Memory: 17.5 MB (12.57%) - LeetSync

--- 3206-find-common-elements-between-two-arrays/find-common-elements-between-two-arrays.py
+++ 3206-find-common-elements-between-two-arrays/find-common-elements-between-two-arrays.py
@@ -3,12 +3,12 @@ class Solution:
         ans= []
         count = 0
         for i in nums1:
-            if i in nums2:
+            if i in set(nums2):
                 count+=1
         ans.append(count)
         count = 0
         for i in nums2:
-            if i in nums1:
+            if i in set(nums1):
                 count+=1
         ans.append(count)
         return ans
