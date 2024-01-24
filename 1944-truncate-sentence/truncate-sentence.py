67da3b021508d85773f908035b75a97717497888
Rahul Asati <89442495+RahulAsati123@users.noreply.github.com>
2024-01-24 16:18:12 +0530

    Time: 30 ms (91.98%) | Memory: 16.6 MB (57.78%) - LeetSync

--- 1944-truncate-sentence/truncate-sentence.py
+++ 1944-truncate-sentence/truncate-sentence.py
@@ -1,13 +1,14 @@
 class Solution:
     def truncateSentence(self, s: str, k: int) -> str:
-        arr = s.split()
+        space = 0
         ans = ""
-        for i in range(k):
-            if i==k-1:
-                ans+=arr[i]
-            else:
-                ans+=arr[i]
-                ans += " "
+        for i in range(len(s)):
+            if s[i] == " ":
+                space+=1
+            if space == k:
+                break
+           
+            ans+=s[i]
         return ans
-
+        
         
