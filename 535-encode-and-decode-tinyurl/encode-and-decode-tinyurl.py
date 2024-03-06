6861b5e99e78f98f911f627a5d2e52ef78bfc85a
Rahul Asati <89442495+RahulAsati123@users.noreply.github.com>
2024-03-06 11:54:20 +0530

    Time: 44 ms (14.66%) | Memory: 16.5 MB (84.26%) - LeetSync

--- 535-encode-and-decode-tinyurl/encode-and-decode-tinyurl.py
+++ 535-encode-and-decode-tinyurl/encode-and-decode-tinyurl.py
@@ -1,13 +1,26 @@
 class Codec:
 
     def encode(self, longUrl: str) -> str:
-        return longUrl
+        m = {}
+        for i in longUrl:
+            m[i] = chr(ord(i)+1)
+        en_string = ""
+        for i in longUrl:
+            en_string += m[i]
+        return en_string
+        
+        
         """Encodes a URL to a shortened URL.
         """
         
 
     def decode(self, shortUrl: str) -> str:
-        return shortUrl
+        de_string = ""
+        for i in shortUrl:
+            de_string += chr(ord(i)-1)
+        return de_string
+       
+    
         """Decodes a shortened URL to its original URL.
         """
         
